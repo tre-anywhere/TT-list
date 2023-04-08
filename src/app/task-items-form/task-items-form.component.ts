64bd9b23507aff86d39565a205ed5656e69e7810
tre-anywhere <tre.grisby@anywhere.re>
2023-04-07 17:04:30 -0700

    [wip] delete a task
    working on Delete task

--- src/app/task-items-form/task-items-form.component.ts
+++ src/app/task-items-form/task-items-form.component.ts
@@ -10,6 +10,7 @@ import { TodoServiceService } from '../data/todo-service.service';
 export class TaskItemsFormComponent {
   values = new Task();
   listFilter: string = 'cart';
+  removeTaskNumber: number = 1;
 
   constructor(private todoService: TodoServiceService) {}
 
@@ -31,11 +32,30 @@ export class TaskItemsFormComponent {
     let elemItem = document.getElementById('orderedItemList');
     let newItem = document.createElement('li');
 
-    // without type info
     newItem.textContent += this.listFilter;
-    newItem.className = '';
+    newItem.className = 'taskItem';
 
     elemItem?.appendChild(newItem);
-    console.log('onKey complete');
+  }
+
+  removeTask() {
+    let listElem = document.querySelectorAll('.taskItem');
+    // let removeTaskNumber = document.getElementById('removeTaskNumber');
+
+    // console.log('removeTask fires!!');
+    console.log('STARTING... listElem.length', listElem);
+    // console.log('removeTaskNumber', this.removeTaskNumber);
+
+    // listElem.forEach(e =>(e.remove));
+
+    for (let i = 0; i < listElem.length; i++) {
+      // console.log(`listElem[${i}]`, listElem[i]);
+      console.log(`index ${i}`);
+      console.log(`this.removeTaskNumber ${this.removeTaskNumber}`);
+
+      if (i === this.removeTaskNumber - 1) {
+        listElem[i].remove;
+      }
+    }
   }
 }
